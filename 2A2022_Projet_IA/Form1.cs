57576fbc9b81ad136ea68c71f6b34ad9a9511ade
Martin Devreese <devreese.martin@gmail.com>
2020-11-06 14:48:27 +0100

    Ajout des scripts du prof

--- 2A2022_Projet_IA/Form1.cs
+++ 2A2022_Projet_IA/Form1.cs
@@ -16,5 +16,10 @@ namespace _2A2022_Projet_IA
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
