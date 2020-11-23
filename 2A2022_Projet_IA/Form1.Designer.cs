8ea4845cc8027d0f8965d0cf8701859380cfad55
Martin Devreese <devreese.martin@gmail.com>
2020-11-23 09:12:09 +0100

    added gui elements

--- 2A2022_Projet_IA/Form1.Designer.cs
+++ 2A2022_Projet_IA/Form1.Designer.cs
@@ -35,26 +35,27 @@ namespace _2A2022_Projet_IA
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.colorComboBox = new System.Windows.Forms.ComboBox();
+            this.casGroupBox = new System.Windows.Forms.GroupBox();
+            this.colorLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.casGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // listBox1
             // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Left;
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(39, 233);
-            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(159, 116);
+            this.listBox1.Size = new System.Drawing.Size(138, 373);
             this.listBox1.TabIndex = 0;
-            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(39, 30);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(19, 24);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "Cas 1";
             this.button1.UseVisualStyleBackColor = true;
@@ -62,10 +63,9 @@ namespace _2A2022_Projet_IA
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(39, 65);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Location = new System.Drawing.Point(19, 65);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 2;
             this.button2.Text = "Cas 2";
             this.button2.UseVisualStyleBackColor = true;
@@ -73,10 +73,9 @@ namespace _2A2022_Projet_IA
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(39, 101);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Location = new System.Drawing.Point(19, 111);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(100, 28);
+            this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 3;
             this.button3.Text = "Cas 3";
             this.button3.UseVisualStyleBackColor = true;
@@ -84,29 +83,57 @@ namespace _2A2022_Projet_IA
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(328, 30);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Location = new System.Drawing.Point(299, 36);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(400, 369);
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
             // 
+            // colorComboBox
+            // 
+            this.colorComboBox.FormattingEnabled = true;
+            this.colorComboBox.Location = new System.Drawing.Point(159, 226);
+            this.colorComboBox.Name = "colorComboBox";
+            this.colorComboBox.Size = new System.Drawing.Size(121, 21);
+            this.colorComboBox.TabIndex = 5;
+            // 
+            // casGroupBox
+            // 
+            this.casGroupBox.Controls.Add(this.button1);
+            this.casGroupBox.Controls.Add(this.button2);
+            this.casGroupBox.Controls.Add(this.button3);
+            this.casGroupBox.Location = new System.Drawing.Point(159, 12);
+            this.casGroupBox.Name = "casGroupBox";
+            this.casGroupBox.Size = new System.Drawing.Size(121, 156);
+            this.casGroupBox.TabIndex = 6;
+            this.casGroupBox.TabStop = false;
+            this.casGroupBox.Text = "Cas de navigation";
+            // 
+            // colorLabel
+            // 
+            this.colorLabel.AutoSize = true;
+            this.colorLabel.Location = new System.Drawing.Point(159, 207);
+            this.colorLabel.Name = "colorLabel";
+            this.colorLabel.Size = new System.Drawing.Size(85, 13);
+            this.colorLabel.TabIndex = 7;
+            this.colorLabel.Text = "Couleur du tracé";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(795, 447);
+            this.ClientSize = new System.Drawing.Size(638, 373);
+            this.Controls.Add(this.colorLabel);
+            this.Controls.Add(this.casGroupBox);
+            this.Controls.Add(this.colorComboBox);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.listBox1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.casGroupBox.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -117,6 +144,9 @@ namespace _2A2022_Projet_IA
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox colorComboBox;
+        private System.Windows.Forms.GroupBox casGroupBox;
+        private System.Windows.Forms.Label colorLabel;
     }
 }
 
